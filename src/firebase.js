023357be1a9e4ec306231522c238e80f06ef1b5d
nkoubamarius <nkoubamarius@gmail.com>
2020-09-16 08:40:49 +0100

    Building Tinder clone with Reactjs

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCEqSwFU1Zs9NP3BEvIE48ksrzy_zp9G2k",
+    authDomain: "tinder-clone-93555.firebaseapp.com",
+    databaseURL: "https://tinder-clone-93555.firebaseio.com",
+    projectId: "tinder-clone-93555",
+    storageBucket: "tinder-clone-93555.appspot.com",
+    messagingSenderId: "236572923597",
+    appId: "1:236572923597:web:35d6ee9d1bc854f3403b84",
+    measurementId: "G-HK3P4SMW6Z"
+  };
